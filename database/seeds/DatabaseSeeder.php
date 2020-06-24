d9bd725a3ac368bec81b076d435b5b504acac597
DUYDV <duy.dang@ntq-solution.com.vn>
2020-06-24 17:44:28 +0700

    firt commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+Class ArticlesTableSeeder extends Seeder
+{
+    public function  run()
+    {
+        // Let's truncate our existing records to start from scratch.ß
+    }
+}
